9102f84238d0cdd2e62f7ed18944d7ed6a20d42b
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2021-08-17 13:42:26 +0300

    home assigment

---
+++ main.js
@@ -0,0 +1,109 @@
+let first = "david"
+let last = "davidov"
+let full = "david davidov"
+
+// no 1+2
+// function printLongestName (firstName, lastName){    
+//     console.log(first.indexOf("a"));    
+//     console.log(last.indexOf("api"))
+
+
+//     if (firstName.length<lastName.length) {
+//         return lastName
+//     }
+//     else { return firstName}
+
+// }
+// console.log(printLongestName(first, last))
+
+// no3
+// function checkIfExist(firstName, lastName) {
+//     if (firstName.indexOf("a") != -1){
+//     return "true"
+// }
+
+// if (lastName.indexOf("api") != -1){
+//     return "true"
+// }
+//     else {return "false"}
+// }
+
+// console.log(checkIfExist(first, last))
+
+// no4
+// function check(string) {
+//     console.log( string.length);
+//     console.log( string.match(" ") )
+//     console.log(string.substr(0,5) + " " + string.substr(6,12))
+
+// }
+
+// console.log(check(full))
+
+// no5
+// function checkLegth (input) {
+//     if ( input.length<3) {
+//     return "yes"
+//     }
+// else {return "no"}
+// }
+// let state = "israel"
+// console.log(checkLegth(state));
+
+// no6
+// function stringAndNote(string, i) {
+//     if (string.length>6) {
+//         return string
+//     }
+// else {return string.indexOf(i)}
+// }
+
+// let input = "Israel"
+// console.log(stringAndNote(input))
+
+// no7
+// function checkNote(string, note){
+//     if (string.match(note)) {
+//         return note
+//     }
+//     else {return string}
+// }
+
+// let note = "a"
+// let string = "codding"
+// console.log(checkNote(string, note))
+
+// no8
+// let first2 = "israel"
+// let last2 = "israeli"
+// function formalName (firstName, lastName){
+//     return firstName[0]+ ". "  + lastName.toUpperCase()
+// }
+
+// console.log(formalName(first2, last2));
+
+// no9
+// let last1= "israeli"
+// let last2= "gavrieli"
+
+// function longestLastName(name1, name2) {
+//     if ( name1.length<name2.length) {
+//         console.log(name1.toUpperCase())
+//         return name2
+//     }
+// }
+
+// console.log(longestLastName(last1, last2));
+
+// no10
+// let note = "i"
+// let name = "jonathan"
+
+// function checkNoteInName (name, note) {
+//     if (name.match(note)>=0) {
+//         return name
+//     }
+//       alert ("the note exist in the name")
+// }
+
+// console.log(checkNoteInName(name, note));
